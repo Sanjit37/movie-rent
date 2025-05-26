21889f1da9c2fdfaacd105278261be912f7a46a8
Sanjit <sanjitprad@gmail.com>
2025-05-26 18:49:19 +0530

    Sanjit: Added health endpoint

--- cmd/main.go
+++ cmd/main.go
@@ -5,5 +5,9 @@ import "github.com/gin-gonic/gin"
 func main() {
 	route := gin.Default()
 
+	route.GET("/health", func(c *gin.Context) {
+		c.JSON(200, gin.H{"Greetings": "Hello world"})
+	})
+
 	route.Run(":8080")
 }
